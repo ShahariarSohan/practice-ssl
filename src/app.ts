8c744de91a46c31eb836ca7e55d3134639b13e2b
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-10 15:10:46 +0600

    global error handled and reduced DRY in controller

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
-
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1",router)
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
-        message:"This is a Tour Management System"
-    })
-})
+  res.status(200).json({
+    message: "This is a Tour Management System",
+  });
+});
 
+app.use(globalErrorHandler);
+app.use(notFound);
 export default app;
