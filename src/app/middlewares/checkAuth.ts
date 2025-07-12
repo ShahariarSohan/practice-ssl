3498a90b768c888653e88b3e9013fe38a4eac771
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-12 15:20:15 +0600

    checkAuth middleware added

---
+++ src/app/middlewares/checkAuth.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response} from "express";
+import { JwtPayload } from "jsonwebtoken";
+import { verifyToken } from "../utils/jwt";
+import { envVars } from "../config/env";
+import AppError from "../errorHelpers/appError";
+
+
+export const checkAuth =
+  (...authRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+      if (!accessToken) {
+        throw new AppError(403, "No token received");
+      }
+      const verifiedToken = verifyToken(
+        accessToken,
+        envVars.JWT_ACCESS_SECRET
+      ) as JwtPayload;
+
+      if (!verifiedToken) {
+        console.log(verifiedToken);
+        throw new AppError(403, `You are not authorized ${verifiedToken}`);
+      }
+
+      if (!authRoles.includes(verifiedToken.role)) {
+        throw new AppError(403, "You are not permitted to view this route");
+      }
+      next();
+    } catch (error) {
+      console.log("JWT Error", error);
+      next(error);
+    }
+  };
