56bec23949a57dca7cdc71063ee984f372ab4661
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-13 20:10:13 +0600

    logout system added

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -5,19 +5,14 @@ import sendResponse from "../../utils/sendResponse";
 import { NextFunction, Request, Response } from 'express';
 import { authServices } from './auth.service';
 import AppError from '../../errorHelpers/appError';
+import { setAuthCookie } from '../../utils/setCookie';
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
    
         const loginInfo = await authServices.credentialsLogin(req.body)
-    res.cookie("refreshToken", loginInfo.refreshToken, {
-          httpOnly:true,
-          secure:false
-    })
-    res.cookie("accessToken", loginInfo.accessToken, {
-      httpOnly: true,
-      secure:false
-    })
+    
+    setAuthCookie(res,loginInfo)
     sendResponse(res, {
       statusCode: httpStatus.CREATED,
       success: true,
@@ -33,17 +28,39 @@ const getNewAccessToken = catchAsync(
       throw new AppError(httpStatus.BAD_REQUEST,"No refresh token received in cookies")
     }
         const tokenInfo = await authServices.getNewAccessToken(refreshToken)
-        
+        setAuthCookie(res,tokenInfo.accessToken)
     sendResponse(res, {
       statusCode: httpStatus.CREATED,
       success: true,
-      message: "User Logged In Successfully",
+      message: "New access token retrieved  Successfully",
       data: tokenInfo,
+    });
+  }
+);
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite:"lax",
+    })
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite:"lax",
+    })
+    sendResponse(res, {
+      statusCode: httpStatus.CREATED,
+      success: true,
+      message: "User logged out Successfully",
+      data: null,
     });
   }
 );
 
 export const authControllers = {
   credentialsLogin,
-  getNewAccessToken
+  getNewAccessToken,
+  logout
 }
