c4fab899a12f730ae8b7c8e2a92ebc7ce7d222fb
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-14 15:25:38 +0600

    google login added

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,11 +1,17 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { authControllers } from "./auth.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
+import passport from "passport";
+
 
 const router = Router();
 router.post("/login",authControllers.credentialsLogin)
 router.post("/refresh-token", authControllers.getNewAccessToken)
 router.post("/logout", authControllers.logout)
-router.post("/reset-password",checkAuth(...Object.values(Role)),authControllers.resetPassword)
+router.post("/reset-password", checkAuth(...Object.values(Role)), authControllers.resetPassword)
+router.get("/google", async (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate("google",{scope:["profile","email"]})(req,res,next)
+})
+router.get("/google/callback",passport.authenticate("google",{failureRedirect:"/login"}),authControllers.googleCallbackController)
 export const authRoutes = router;
