23cc87b4f5003a679422f9dd1a9051b15d76125b
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-19 09:38:19 +0600

    seaching and filtering added in tour

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -6,16 +6,15 @@ import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
 
 const router = Router();
-
-router.post(
-  "/register",
-  validateRequest(createUserZodSchema),
-  userControllers.createUser
-);
 router.get(
   "/",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
   userControllers.getAllUser
+);
+router.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  userControllers.createUser
 );
 router.patch(
   "/:id",
